56cc095c41ea75b668395d2dfaf29f1906fe3755
Half People <56476339+Half-People@users.noreply.github.com>
2022-09-01 16:35:44 +0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	system("color 6D");
+	std::cout << "By HalfPeople Studio";
+	std::cout << "\n\n--------------asciiÂë±í--------------\n\n";
+	for (size_t i = 0; i < 129; i++)//129
+	{
+
+		std::cout << "\n|            " << i << "       " << (char)i << "            |";
+	}
+
+
+
+	std::cout << "\n\n--------------The End--------------";
+	while (true)
+	{
+
+	}
+}
